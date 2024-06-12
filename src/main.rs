59937fd057515aa26329f869f11a3326e8efd452
dVeon-loch <39005321+dVeon-loch@users.noreply.github.com>
2024-06-12 15:42:41 +0200

    Add simple file handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 3 {
+        panic!("Enter both the search string and the file name!");
+    }
+
     let query = &args[1];
     let file_path = &args[2];
-    
+
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
