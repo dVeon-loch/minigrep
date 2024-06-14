89ca2cc5173a50832d96d39e4812b99de372ee07
dVeon-loch <39005321+dVeon-loch@users.noreply.github.com>
2024-06-14 10:11:49 +0200

    Use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -6,26 +6,20 @@ pub struct Config {
     pub ignore_case: bool,
 }
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments. Enter both the search string and the file name!");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
-        let ignore_case: bool;
-        if args.len() > 3 {
-            if args.contains(&String::from("--nocase")) {
-                ignore_case = true;
-            } else if args.contains(&String::from("--case")) {
-                ignore_case = false;
-            } else {
-                ignore_case = env::var("IGNORE_CASE").is_ok();
-            }
-        } else {
-            ignore_case = env::var("IGNORE_CASE").is_ok();
-        }
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
             query,
@@ -51,27 +45,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .into_iter()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut containing_lines: Vec<&'a str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            containing_lines.push(line);
-        }
-    }
-
-    containing_lines
+    contents
+        .lines()
+        .into_iter()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 #[cfg(test)]
